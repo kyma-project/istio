2231a270204304d2f859faac24786007797f977e
Bartosz Chwila <bartosz.chwila@sap.com>
2022-12-13 16:03:54 +0100

    Update tests for use on PROW

--- operator/pkg/lib/sidecars/test/restart_test.go
+++ operator/pkg/lib/sidecars/test/restart_test.go
@@ -1,7 +1,12 @@
 package test
 
 import (
+	"fmt"
+	"io"
+	"io/fs"
 	"os"
+	"path/filepath"
+	"regexp"
 	"testing"
 
 	_ "embed"
@@ -48,7 +53,7 @@ func TestProxyReset(t *testing.T) {
 	suiteExitCode := suite.Run()
 
 	if os.Getenv(exportResultVar) == "true" {
-		generateHTMLReport()
+		generateReport()
 	}
 
 	if suiteExitCode != 0 {
@@ -56,15 +61,17 @@ func TestProxyReset(t *testing.T) {
 	}
 }
 
-func generateHTMLReport() {
+func generateReport() {
+	htmlOutputDir := "reports/"
+
 	html := gocure.HTML{
 		Config: html.Data{
 			InputJsonPath:    "cucumber-report.json",
-			OutputHtmlFolder: "reports/",
-			Title:            "Kyma Istio component tests",
+			OutputHtmlFolder: htmlOutputDir,
+			Title:            "Kyma API-Gateway component tests",
 			Metadata: models.Metadata{
-				TestEnvironment: "fake",
 				Platform:        runtime.GOOS,
+				TestEnvironment: "Gardener GCP",
 				Parallel:        "Scenarios",
 				Executed:        "Remote",
 				AppVersion:      "main",
@@ -76,4 +83,81 @@ func generateHTMLReport() {
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
+
+	err = filepath.Walk("reports", func(path string, info fs.FileInfo, err error) error {
+		if path == "reports" {
+			return nil
+		}
+
+		data, err1 := os.ReadFile(path)
+		if err1 != nil {
+			return err
+		}
+
+		//Format all patterns like "&lt" to not be replaced later
+		find := regexp.MustCompile(`&\w\w`)
+		formatted := find.ReplaceAllFunc(data, func(b []byte) []byte {
+			return []byte{b[0], ' ', b[1], b[2]}
+		})
+
+		err = os.WriteFile(path, formatted, fs.FileMode(02))
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	if artifactsDir, ok := os.LookupEnv("ARTIFACTS"); ok {
+		err = filepath.Walk("reports", func(path string, info fs.FileInfo, err error) error {
+			if path == "reports" {
+				return nil
+			}
+
+			_, err1 := copy(path, fmt.Sprintf("%s/report.html", artifactsDir))
+			if err1 != nil {
+				return err1
+			}
+			return nil
+		})
+
+		if err != nil {
+			log.Fatalf(err.Error())
+		}
+
+		_, err = copy("./junit-report.xml", fmt.Sprintf("%s/junit-report.xml", artifactsDir))
+		if err != nil {
+			log.Fatalf(err.Error())
+		}
+	}
+
+}
+
+func copy(src, dst string) (int64, error) {
+	sourceFileStat, err := os.Stat(src)
+	if err != nil {
+		return 0, err
+	}
+
+	if !sourceFileStat.Mode().IsRegular() {
+		return 0, fmt.Errorf("%s is not a regular file", src)
+	}
+
+	source, err := os.Open(src)
+	if err != nil {
+		return 0, err
+	}
+	defer source.Close()
+
+	destination, err := os.Create(dst)
+	if err != nil {
+		return 0, err
+	}
+	defer destination.Close()
+	nBytes, err := io.Copy(destination, source)
+	return nBytes, err
 }

65100ffb2cd03ff509df8255bbbbbfb234777203
Tim Riffer <tim.riffer@sap.com>
2023-02-09 15:50:36 +0100

    Remove local junit report generation

--- operator/internal/tests/ginkgo_reporter.go
+++ operator/internal/tests/ginkgo_reporter.go
@@ -20,17 +20,5 @@ func GenerateGinkgoJunitReport(reportName string, report types.Report) {
 		if err != nil {
 			logger.Error(err, "Junit Report Generation Error")
 		}
-	} else {
-		if err := os.MkdirAll("../../../reports", 0755); err != nil {
-			logger.Error(err, "could not create directory")
-		}
-
-		reportsFilename := fmt.Sprintf("%s/junit-%s.xml", "../../../reports", reportName)
-		logger.Info("Generating reports at", "location", reportsFilename)
-		err := reporters.GenerateJUnitReport(report, reportsFilename)
-
-		if err != nil {
-			logger.Error(err, "Junit Report Generation Error")
-		}
 	}
 }

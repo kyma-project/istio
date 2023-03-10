3f9b4f06083cc2f67c0f475525b4a02156efdc73
Bartosz Chwila <103247439+barchw@users.noreply.github.com>
2023-03-10 14:42:21 +0100

    Update istio_controller.go

--- controllers/istio_controller.go
+++ controllers/istio_controller.go
@@ -40,7 +40,7 @@ import (
 )
 
 var (
-	defaultIstioOperatorPath = "manifests/default-istio-operator-k3d.yaml"
+	defaultIstioOperatorPath = "manifests/default-istio-operator-cluster.yaml"
 	workingDir               = "/tmp"
 )
 

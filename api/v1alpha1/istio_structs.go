b63cf2411fa57e7ea5e3a898953f33531738ec36
Chwila <bartosz.chwila@sap.com>
2023-05-24 12:27:10 +0200

    Add docu

--- api/v1alpha1/istio_structs.go
+++ api/v1alpha1/istio_structs.go
@@ -13,10 +13,14 @@ type Config struct {
 }
 
 type Components struct {
-	Pilot           *IstioComponent   `json:"pilot,omitempty"`
+	// Pilot defines component configuration for Istiod
+	Pilot *IstioComponent `json:"pilot,omitempty"`
+	// IngressGateways defines component configurations for Istio Ingress Gateways
 	IngressGateways []*IstioComponent `json:"ingressGateways,omitempty"`
-	Cni             *CniComponent     `json:"cni,omitempty"`
-	Proxy           *ProxyComponent   `json:"proxy,omitempty"`
+	// Cni defines component configuration for Istio CNI DaemonSet
+	Cni *CniComponent `json:"cni,omitempty"`
+	// Proxy defines component configuration for Istio proxy sidecar
+	Proxy *ProxyComponent `json:"proxy,omitempty"`
 }
 
 // KubernetesResourcesConfig is a subset of https://istio.io/latest/docs/reference/config/istio.operator.v1alpha1/#KubernetesResourcesSpec
@@ -26,40 +30,48 @@ type KubernetesResourcesConfig struct {
 	Resources *Resources `json:"resources,omitempty"`
 }
 
+// ProxyComponent defines configuration for Istio proxies
 type ProxyComponent struct {
 	// +kubebuilder:validation:Required
 	K8S *ProxyK8sConfig `json:"k8s"`
 }
 
+// ProxyK8sConfig is a subset of https://istio.io/latest/docs/reference/config/istio.operator.v1alpha1/#KubernetesResourcesSpec
 type ProxyK8sConfig struct {
 	Resources *Resources `json:"resources,omitempty"`
 }
 
+// CniComponent defines configuration for CNI Istio component
 type CniComponent struct {
 	// +kubebuilder:validation:Required
 	K8S *CniK8sConfig `json:"k8s"`
 }
 
+// CniK8sConfig is a subset of https://istio.io/latest/docs/reference/config/istio.operator.v1alpha1/#KubernetesResourcesSpec
 type CniK8sConfig struct {
 	Affinity  *v1.Affinity `json:"affinity,omitempty"`
 	Resources *Resources   `json:"resources,omitempty"`
 }
 
+// HPASpec defines configuration for HorizontalPodAutoscaler
 type HPASpec struct {
 	MaxReplicas *int32 `json:"maxReplicas,omitempty"`
 	MinReplicas *int32 `json:"minReplicas,omitempty"`
 }
 
+// IstioComponent defines configuration for generic Istio component (ingress gateway, istiod)
 type IstioComponent struct {
 	// +kubebuilder:validation:Required
 	K8s *KubernetesResourcesConfig `json:"k8s"`
 }
 
+// Strategy defines rolling update strategy
 type Strategy struct {
 	// +kubebuilder:validation:Required
 	RollingUpdate *RollingUpdate `json:"rollingUpdate"`
 }
 
+// RollingUpdate defines configuration for rolling updates: https://kubernetes.io/docs/concepts/workloads/controllers/deployment/#rolling-update-deployment
 type RollingUpdate struct {
 	// +kubebuilder:validation:XIntOrString
 	MaxSurge *intstr.IntOrString `json:"maxSurge" protobuf:"bytes,2,opt,name=maxSurge"`
@@ -67,6 +79,7 @@ type RollingUpdate struct {
 	MaxUnavailable *intstr.IntOrString `json:"maxUnavailable" protobuf:"bytes,1,opt,name=maxUnavailable"`
 }
 
+// Resources define Kubernetes resources configuration: https://kubernetes.io/docs/concepts/configuration/manage-resources-containers/
 type Resources struct {
 	Limits   *ResourceClaims `json:"limits,omitempty"`
 	Requests *ResourceClaims `json:"requests,omitempty"`

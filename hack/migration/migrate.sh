4ab56110917808fbab17792b65567747ff38609d
Tim Riffer <tim.riffer@sap.com>
2023-06-12 16:17:24 +0200

    Add migration script

---
+++ hack/migration/migrate.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+
+# Verify Istio module template is available on cluster
+istio_module_template_count=$(kubectl get moduletemplates.operator.kyma-project.io -A --output json | jq -j '.items | length')
+
+if [ "$istio_module_template_count" -eq 0 ]; then
+  echo "Istio module template is not available on cluster"
+  exit 1
+fi
+
+# Fetch Kyma CR name managed by lifecycle-manager
+kyma_cr_name=$(kubectl get kyma -n kyma-system -l operator.kyma-project.io/managed-by=lifecycle-manager --no-headers -o custom-columns=":metadata.name")
+
+# Fetch all modules, if modules is not defined, fallback to an empty array and count the number modules that have the name "istio"
+istio_module_count=$(kubectl get kyma "$kyma_cr_name" -n kyma-system -o json | jq '.spec.modules | if . == null then [] else . end | map(. | select(.name=="istio")) | length')
+
+if [  "$istio_module_count" -gt 0 ]; then
+  echo "Istio module already present on Kyma CR, skipping migration"
+  exit 0
+fi
+
+# Check if Istio CR is already present on Kubernetes cluster
+istio_crs=$(kubectl get istios -n kyma-system --output json | jq '.items | length')
+if [ "$istio_crs" -gt 0 ]; then
+  echo "Istio CR found, proceeding with migration by adding Istio module to Kyma CR and setting customResourcePolicy to Ignore"
+  kubectl patch kyma "$kyma_cr_name" -n kyma-system --type='json' -p='[{"op": "add", "path": "/spec/modules", "value": [{"name": "istio", "customResourcePolicy": "Ignore"}] }]'
+  #TODO: As far as I remember we should not remove the customResourcePolicy after migration, is this still correct?
+else
+  echo "Istio CR not found, proceeding with migration by adding Istio module to Kyma CR"
+  kubectl patch kyma "$kyma_cr_name" -n kyma-system --type='json' -p='[{"op": "add", "path": "/spec/modules", "value": [{"name": "istio"}] }]'
+fi
+
+echo "Istio CR migration completed successfully"
+exit 0
---
+++ hack/migration/readme.md
@@ -0,0 +1,4 @@
+# Migration documentation
+
+## Notes
+- There can be a race condition during the module migration from reconciler to Lifecycle-Manager. Details and a proposed process how to avoid this can be found in this [Slack thread](https://sap-btp.slack.com/archives/C042CAZDZDX/p1685003293524069?thread_ts=1685000510.183569&cid=C042CAZDZDX).

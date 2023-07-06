feb40fb819c8e7b9f0356621741213560d26f710
Vladimir Videlov <vladimir.videlov@sap.com>
2023-07-06 17:46:38 +0200

    ingress-gateway package

---
+++ internal/ingress-gateway/deployment.go
@@ -0,0 +1,62 @@
+package ingressgateway
+
+import (
+	"context"
+	"time"
+
+	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	operatorv1alpha1 "github.com/kyma-project/istio/operator/api/v1alpha1"
+)
+
+const (
+	namespace      string = "istio-system"
+	name           string = "istio-ingressgateway"
+	annotationName string = "reconciler.kyma-project.io/lastRestartDate"
+)
+
+func RestartDeployment(ctx context.Context, k8sClient client.Client) error {
+	deployment := appsv1.Deployment{}
+	err := k8sClient.Get(ctx, types.NamespacedName{Namespace: namespace, Name: name}, &deployment)
+	if err != nil {
+		return err
+	}
+
+	if len(deployment.Spec.Template.Annotations) == 0 {
+		deployment.Spec.Template.Annotations = make(map[string]string)
+	}
+	deployment.Spec.Template.Annotations[annotationName] = time.Now().Format(time.RFC3339)
+
+	return k8sClient.Update(ctx, &deployment)
+}
+
+func NeedsRestart(ctx context.Context, client client.Client, istioCR *operatorv1alpha1.Istio) (bool, error) {
+	numTrustedProxies, err := GetNumTrustedProxyFromIstioCM(ctx, client)
+	if err != nil {
+		return false, err
+	}
+
+	// Restart to default if no IstioCR is present
+	if numTrustedProxies != nil && istioCR == nil {
+		return true, nil
+	}
+
+	if istioCR == nil {
+		return false, nil
+	}
+
+	isNewNotNil := (istioCR.Spec.Config.NumTrustedProxies != nil)
+	isOldNotNil := (numTrustedProxies != nil)
+	if isNewNotNil && isOldNotNil {
+		if *istioCR.Spec.Config.NumTrustedProxies != *numTrustedProxies {
+			return true, nil
+		}
+	}
+	if isNewNotNil != isOldNotNil {
+		return true, nil
+	}
+
+	return false, nil
+}

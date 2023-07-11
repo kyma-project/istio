940c63297d3e51c92afa0325a77f2a0e53f3fbdc
Marek Kolodziejczak <marek.kolodziejczak@sap.com>
2023-07-11 08:46:35 +0200

    Fix report generation

--- tests/integration/test_report.go
+++ tests/integration/test_report.go
@@ -81,15 +81,18 @@ func generateReport(suiteName string) error {
 			return err
 		}
 
-		_, err = copyReport("./junit-main-report.xml", fmt.Sprintf("%s/junit-main-report-%s.xml", artifactsDir, suiteName))
-		if err != nil {
-			return err
+		if suiteName == "istio-main-suite" {
+			_, err = copyReport("./junit-main-report.xml", fmt.Sprintf("%s/junit-main-report-%s.xml", artifactsDir, suiteName))
+			if err != nil {
+				return err
+			}
+		} else if suiteName == "istio-upgrade-suite" {
+			_, err = copyReport("./junit-upgrade-report.xml", fmt.Sprintf("%s/junit-upgrade-report-%s.xml", artifactsDir, suiteName))
+			if err != nil {
+				return err
+			}
 		}
 	
-		_, err = copyReport("./junit-upgrade-report.xml", fmt.Sprintf("%s/junit-upgrade-report-%s.xml", artifactsDir, suiteName))
-		if err != nil {
-			return err
-		}
 	}
 	return nil
 }

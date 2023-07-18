48363675132bf02c2566e24aa3e614ea84c531f4
Marek Kolodziejczak <marek.kolodziejczak@sap.com>
2023-07-18 11:29:33 +0200

    Improve deploy latest release script

--- tests/integration/scripts/deploy-latest-release-to-cluster.sh
+++ tests/integration/scripts/deploy-latest-release-to-cluster.sh
@@ -4,10 +4,19 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-RELEASE_INFO=$(curl -L \
-                 -H "Accept: application/vnd.github+json" \
-                 -H "X-GitHub-Api-Version: 2022-11-28" \
-                 https://api.github.com/repos/kyma-project/istio/releases/latest)
+BRANCH=$(git branch --show-current)
 
-RELEASE_MANIFEST_URL=$(echo "$RELEASE_INFO" | jq -r '.assets[] | select(.name == "istio-manager.yaml") | .browser_download_url')
-curl -L "$RELEASE_MANIFEST_URL" | kubectl apply -f -
+if [ "$BRANCH" == "main" ]
+then
+  TAG=$(git describe --tags --abbrev=0)
+  RELEASE_MANIFEST_URL="https://github.com/kyma-project/istio/releases/download/${TAG}/istio-default-cr.yaml"
+ curl -L "$RELEASE_MANIFEST_URL" | kubectl apply -f -
+else
+ RELEASE_INFO=$(curl -L \
+                  -H "Accept: application/vnd.github+json" \
+                  -H "X-GitHub-Api-Version: 2022-11-28" \
+                  https://api.github.com/repos/kyma-project/istio/releases/latest)
+
+ RELEASE_MANIFEST_URL=$(echo "$RELEASE_INFO" | jq -r '.assets[] | select(.name == "istio-manager.yaml") | .browser_download_url')
+ curl -L "$RELEASE_MANIFEST_URL" | kubectl apply -f -
+fi

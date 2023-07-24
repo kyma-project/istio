5e84f7f9e2dab1a0a91b0407dbc5a60b9d87a034
Vladimir Videlov <vladimir.videlov@sap.com>
2023-07-24 15:13:54 +0200

    Lowercase error

--- controllers/istio_controller.go
+++ controllers/istio_controller.go
@@ -90,7 +90,7 @@ func (r *IstioReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl
 	if len(existingIstioCRs.Items) > 1 {
 		oldestCr := r.getOldestCR(existingIstioCRs)
 		if istioCR.GetUID() != oldestCr.GetUID() {
-			errNotOldestCR := fmt.Errorf("Only Istio CR %s in %s reconciles the module", oldestCr.GetName(), oldestCr.GetNamespace())
+			errNotOldestCR := fmt.Errorf("only Istio CR %s in %s reconciles the module", oldestCr.GetName(), oldestCr.GetNamespace())
 			return r.terminateReconciliation(ctx, istioCR, described_errors.NewDescribedError(errNotOldestCR, "Stopped Istio CR reconciliation"))
 		}
 	}

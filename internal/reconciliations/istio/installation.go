52cbe42437452aa4729968a7c84ca81fcbc6a45a
Bartosz Chwila <103247439+barchw@users.noreply.github.com>
2023-09-01 11:49:03 +0200

    Update installation.go

--- internal/reconciliations/istio/installation.go
+++ internal/reconciliations/istio/installation.go
@@ -132,7 +132,7 @@ func (i *Installation) Reconcile(ctx context.Context, istioCR operatorv1alpha1.I
 			})
 
 			return istioCR, described_errors.NewDescribedError(fmt.Errorf("could not delete Istio module instance since there are %d customer resources present", len(clientResources)),
-				fmt.Sprintf("Please take a look at kyma-system/istio-controller-manager logs to see more information about the warning")).DisableErrorWrap().SetWarning()
+				"Please take a look at kyma-system/istio-controller-manager logs to see more information about the warning").DisableErrorWrap().SetWarning()
 		}
 
 		err = i.IstioClient.Uninstall(ctx)

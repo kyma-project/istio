7a633b2bc963b0eaa25e1265cdd7e49afdd40902
Chwila <bartosz.chwila@sap.com>
2023-10-18 16:52:09 +0200

    no namespace

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -2,17 +2,10 @@ package metrics
 
 import "github.com/prometheus/client_golang/prometheus"
 
-const (
-	namespace = "kyma"
-	subsystem = "kyma_istio_operator"
-)
-
 func CreateInstallationsCounter() prometheus.Counter {
 	requestCounterOpts := prometheus.CounterOpts{
-		Namespace: namespace,
-		Subsystem: subsystem,
-		Name:      "installations_processed",
-		Help:      "Number of installations that istio controller processed",
+		Name: "installations_processed",
+		Help: "Number of installations that istio controller processed",
 	}
 	installationCounter := prometheus.NewCounter(requestCounterOpts)
 	prometheus.MustRegister(installationCounter)

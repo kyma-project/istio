3b8d0996b909f82b1858483413a322dcd7eaaaab
Tim Riffer <tim.riffer@sap.com>
2023-10-26 13:40:44 +0200

    Fix false hyperscaler test

--- internal/clusterconfig/hyperscaler_test.go
+++ internal/clusterconfig/hyperscaler_test.go
@@ -5,13 +5,20 @@ import (
 	"github.com/kyma-project/istio/operator/internal/clusterconfig"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var _ = Describe("Hyperscaler", func() {
 	Context("IsHyperscalerAWS", func() {
 		It("should be true if hyperscaler is AWS", func() {
 			// given
-			k8sClient := createFakeClient()
+			cm := v1.ConfigMap{
+				ObjectMeta: metav1.ObjectMeta{Name: clusterconfig.ConfigMapShootInfoName, Namespace: clusterconfig.ConfigMapShootInfoNS},
+				Data:       map[string]string{"provider": "aws"},
+			}
+
+			k8sClient := createFakeClient(&cm)
 			// when
 			isAws, err := clusterconfig.IsHyperscalerAWS(context.Background(), k8sClient)
 
@@ -21,8 +28,16 @@ var _ = Describe("Hyperscaler", func() {
 		})
 
 		It("should be false if hyperscaler is not AWS", func() {
+			// given
+			cm := v1.ConfigMap{
+				ObjectMeta: metav1.ObjectMeta{Name: clusterconfig.ConfigMapShootInfoName, Namespace: clusterconfig.ConfigMapShootInfoNS},
+				Data:       map[string]string{"provider": "gcp"},
+			}
+
+			k8sClient := createFakeClient(&cm)
+
 			// when
-			isAws, err := clusterconfig.IsHyperscalerAWS()
+			isAws, err := clusterconfig.IsHyperscalerAWS(context.Background(), k8sClient)
 
 			// then
 			Expect(err).ToNot(HaveOccurred())

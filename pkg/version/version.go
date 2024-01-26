eb23c2c9a249e3693dcea3da2bc37d0f40ce2193
Kamil Kasperski <ressetkk@gmail.com>
2024-01-26 10:26:27 +0100

    Apply suggestions from code review
    Co-authored-by: Tim Riffer <tim.riffer@gmx.de>
    Co-authored-by: Vladimir Videlov <vladimir.videlov@sap.com>

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,7 +1,8 @@
 package version
 
+// variable is set to the correct version by the Dockerfile during build time.
 var version = "dev"
 
-func Get() string {
+func GetModuleVersion() string {
 	return version
 }

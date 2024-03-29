d923e6505643c6c1c5713f8b8b81d6b389661a44
Chwila <bartosz.chwila@sap.com>
2024-03-29 10:46:34 +0100

    Add threshholds

--- tests/performance/scripts/common.js
+++ tests/performance/scripts/common.js
@@ -13,6 +13,11 @@ export const options = {
 
             vus: 100,
             duration: '60s',
+
+            thresholds: {
+                http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+                http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
+            },
         },
         post_scenario: {
             executor: 'constant-vus',
@@ -22,6 +27,11 @@ export const options = {
 
             vus: 100,
             duration: '60s',
+
+            thresholds: {
+                http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+                http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
+            },
         },
     },
 };

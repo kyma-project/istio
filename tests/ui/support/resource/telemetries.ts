dff3947ec50d5ea729a9c6a8cacdd23bdae217bd
Chwila <bartosz.chwila@sap.com>
2024-04-19 13:06:20 +0200

    Update indent

--- tests/ui/support/resource/telemetries.ts
+++ tests/ui/support/resource/telemetries.ts
@@ -1,37 +1,37 @@
 export interface TelemetryCommands {
-        telemetryTypeName(name: string): void
-        telemetryAddAccessLogging(accessLog: AccessLog): void
-        telemetryAddTracing(tracing: Tracing): void
+    telemetryTypeName(name: string): void
+    telemetryAddAccessLogging(accessLog: AccessLog): void
+    telemetryAddTracing(tracing: Tracing): void
 }
 
 export type Tracing = {
-        randomSamplingPercentage: number;
-        provider: TracingProvider;
+    randomSamplingPercentage: number;
+    provider: TracingProvider;
 }
 
 export type TracingProvider = {
-        name: string;
+    name: string;
 }
 
 export type AccessLog = {
-        mode: "CLIENT_AND_SERVER" | "CLIENT" | "SERVER";
-        filterExpression: string;
+    mode: "CLIENT_AND_SERVER" | "CLIENT" | "SERVER";
+    filterExpression: string;
 }
 
 Cypress.Commands.add('telemetryTypeName', (name: string): void => {
-        cy.inputClearAndType('ui5-input[aria-label="Telemetry name"]', name);
+    cy.inputClearAndType('ui5-input[aria-label="Telemetry name"]', name);
 });
 
 Cypress.Commands.add('telemetryAddAccessLogging', (accessLog: AccessLog): void => {
-        cy.addFormGroupItem('[aria-label="expand Access logging configuration"]:visible');
-        cy.chooseComboboxOption('[data-testid="spec.accessLogging.0.match.mode"]', accessLog.mode);
-        cy.inputClearAndType('ui5-input[data-testid="spec.accessLogging.0.filter.expression"]', accessLog.filterExpression);
+    cy.addFormGroupItem('[aria-label="expand Access logging configuration"]:visible');
+    cy.chooseComboboxOption('[data-testid="spec.accessLogging.0.match.mode"]', accessLog.mode);
+    cy.inputClearAndType('ui5-input[data-testid="spec.accessLogging.0.filter.expression"]', accessLog.filterExpression);
 });
 
 Cypress.Commands.add('telemetryAddTracing', (tracing: Tracing): void => {
-        cy.addFormGroupItem('[aria-label="expand Tracing configuration"]:visible');
-        cy.inputClearAndType('ui5-input[data-testid="spec.tracing.0.randomSamplingPercentage"]', tracing.randomSamplingPercentage.toString());
+    cy.addFormGroupItem('[aria-label="expand Tracing configuration"]:visible');
+    cy.inputClearAndType('ui5-input[data-testid="spec.tracing.0.randomSamplingPercentage"]', tracing.randomSamplingPercentage.toString());
 
-        cy.addFormGroupItem('[aria-label="expand Providers"]:visible');
-        cy.inputClearAndType('ui5-input[data-testid="spec.tracing.0.providers.0.name"]', tracing.provider.name);
+    cy.addFormGroupItem('[aria-label="expand Providers"]:visible');
+    cy.inputClearAndType('ui5-input[data-testid="spec.tracing.0.providers.0.name"]', tracing.provider.name);
 });
--- tests/ui/tests/telemetries.spec.ts
+++ tests/ui/tests/telemetries.spec.ts
@@ -2,50 +2,50 @@ import { generateNamespaceName, generateRandomName } from "../support";
 
 context('Telemetries', () => {
 
-        let namespaceName: string;
-        let telemetryName: string;
+    let namespaceName: string;
+    let telemetryName: string;
 
-        beforeEach(() => {
-                namespaceName = generateNamespaceName();
-                telemetryName = generateRandomName("test-telemetry");
-                cy.loginAndSelectCluster();
-                cy.createNamespace(namespaceName);
+    beforeEach(() => {
+        namespaceName = generateNamespaceName();
+        telemetryName = generateRandomName("test-telemetry");
+        cy.loginAndSelectCluster();
+        cy.createNamespace(namespaceName);
+    });
+
+    afterEach(() => {
+        cy.deleteNamespace(namespaceName);
+    });
+
+    it('should create new', () => {
+        cy.navigateToTelemetries(namespaceName);
+        cy.clickCreateButton();
+
+        cy.telemetryTypeName(telemetryName);
+        cy.telemetryAddAccessLogging({
+            mode: "SERVER",
+            filterExpression: "response.code >= 400"
         });
 
-        afterEach(() => {
-                cy.deleteNamespace(namespaceName);
+        cy.telemetryAddTracing({
+            randomSamplingPercentage: 100,
+            provider: {
+                name: "test-provider"
+            }
         });
 
-        it('should create new', () => {
-                cy.navigateToTelemetries(namespaceName);
-                cy.clickCreateButton();
+        cy.clickCreateButton();
 
-                cy.telemetryTypeName(telemetryName);
-                cy.telemetryAddAccessLogging({
-                        mode: "SERVER",
-                        filterExpression: "response.code >= 400"
-                });
+        // Verify the details
+        cy.contains(telemetryName).should('be.visible');
 
-                cy.telemetryAddTracing({
-                        randomSamplingPercentage: 100,
-                        provider: {
-                                name: "test-provider"
-                        }
-                });
+        cy.contains("AccessLogging #1").should('be.visible').click();
+        cy.contains("Tracing #1").should('be.visible').click();
 
-                cy.clickCreateButton();
+        cy.contains("SERVER").should('be.visible');
+        cy.contains("response.code >= 400").should('be.visible');
+        cy.contains("100").should('be.visible');
+        cy.contains("test-provider").should('be.visible');
 
-                // Verify the details
-                cy.contains(telemetryName).should('be.visible');
-
-                cy.contains("AccessLogging #1").should('be.visible').click();
-                cy.contains("Tracing #1").should('be.visible').click();
-
-                cy.contains("SERVER").should('be.visible');
-                cy.contains("response.code >= 400").should('be.visible');
-                cy.contains("100").should('be.visible');
-                cy.contains("test-provider").should('be.visible');
-
-        });
+    });
 });
 

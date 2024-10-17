fa3bf867573c8a28c037e38a5911ce2d14f788ce
nataliasitko <natalia.sitko@sap.com>
2024-10-17 11:43:10 +0200

    update sidear analysis scrypt

--- docs/assets/sidecar-analysis.sh
+++ docs/assets/sidecar-analysis.sh
@@ -47,7 +47,7 @@ if [ -z "$target_namespace" ]; then
         fi
     done
 
-    echo "  Pods labeled with \"sidecar.istio.io/inject=true\" in not labeled namespaces:"
+    echo "  Pods not labeled with \"sidecar.istio.io/inject=true\" in unlabeled namespaces:"
     not_labeled_ns=$(kubectl get ns -l "istio-injection!=disabled, istio-injection!=enabled" -o jsonpath='{.items[*].metadata.name}')
     for ns in $not_labeled_ns
     do
@@ -57,12 +57,15 @@ if [ -z "$target_namespace" ]; then
     done
 else 
     ns_label=$(kubectl get ns $target_namespace -o jsonpath='{.metadata.labels.istio-injection}')
-    echo "Pods out of istio mesh in namespace $target_namespace:"
+    echo "In the namespace $target_namespace, the following Pods are not part of the Istio service mesh:"
     if [ "$ns_label" == "enabled" ]; then
         log_pods_with $target_namespace "sidecar.istio.io/inject=false"
+        echo -e "Reason: The namespace $target_namespace has Istio sidecar proxy injection enabled, but the above Pods are labeled with \"sidecar.istio.io/inject=false\"."
     elif [ "$ns_label" == "disabled" ]; then
         log_pods_with $target_namespace
+        echo -e "Reason: The namespace $target_namespace has Istio sidecar proxy injection disabled, so none of its Pods have been injected with an Istio sidecar proxy."
     else
         log_pods_with $target_namespace "sidecar.istio.io/inject!=true"
+        echo -e "Reason: The namespace $target_namespace is not labeled and the above Pods are not labeled with "sidecar.istio.io/inject!=true"."
     fi
 fi

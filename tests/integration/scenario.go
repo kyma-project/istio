61f4792ec737a96ce68eaf774c7214ad6f09d8e1
Vladimir Videlov <vladimir.videlov@sap.com>
2024-10-21 12:30:57 +0200

    Remove test because it was added just for sane-keeping. Already existing upgrade test should catch errors if manifest can not be applied (#1082)

--- tests/integration/scenario.go
+++ tests/integration/scenario.go
@@ -69,7 +69,7 @@ func upgradeInitScenario(ctx *godog.ScenarioContext) {
 	ctx.Step(`^Istio injection is "([^"]*)" in namespace "([^"]*)"$`, steps.SetIstioInjection)
 	ctx.Step(`^Httpbin application "([^"]*)" deployment is created in namespace "([^"]*)"$`, steps.CreateHttpbinApplication)
 	ctx.Step(`^Application pod "([^"]*)" in namespace "([^"]*)" has Istio proxy "([^"]*)"$`, steps.ApplicationPodShouldHaveIstioProxy)
-	ctx.Step(`^Istio controller has been upgraded with "([^"]*)" manifest and should "([^"]*)"$`, steps.DeployIstioOperator)
+	ctx.Step(`^Istio controller has been upgraded$`, steps.DeployIstioOperator)
 	ctx.Step(`^Application "([^"]*)" in namespace "([^"]*)" has required version of proxy$`, steps.ApplicationPodShouldHaveIstioProxyInRequiredVersion)
 	ctx.Step(`^Container "([^"]*)" of "([^"]*)" "([^"]*)" in namespace "([^"]*)" has required version$`, steps.IstioResourceContainerHasRequiredVersion)
 }

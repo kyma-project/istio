5efc8a209ea8362442bfa9a73e0e1696cf6db9a7
Patryk Strugacz <patryk.strugacz@sap.com>
2024-12-17 10:54:24 +0100

    Add RN, remove restarter

--- controllers/istio_controller.go
+++ controllers/istio_controller.go
@@ -59,7 +59,6 @@ func NewController(mgr manager.Manager, reconciliationInterval time.Duration) *I
 	statusHandler := status.NewStatusHandler(mgr.GetClient())
 	restarters := []restarter.Restarter{
 		restarter.NewIngressGatewayRestarter(mgr.GetClient(), []filter.IngressGatewayPredicate{}, statusHandler),
-		restarter.NewEgressGatewayRestarter(mgr.GetClient(), []filter.EgressGatewayPredicate{}, statusHandler),
 		restarter.NewSidecarsRestarter(mgr.GetLogger(), mgr.GetClient(), &merger, sidecars.NewProxyResetter(), statusHandler),
 	}
 

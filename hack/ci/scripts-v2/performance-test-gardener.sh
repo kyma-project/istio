434291fdde9f858281c2d9211940350187f15b53
Joanna Bodora <asia.bodora@sap.com>
2025-02-21 14:04:27 +0100

    Create GH workflow.

---
+++ hack/ci/scripts-v2/performance-test-gardener.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+# Description: This script runs given performance tests on a real Gardener cluster
+# It deploys Istio module and then runs performance tests make target
+# It requires the following env variables:
+# - IMG - Istio module image to be deployed (by make deploy)
+# - CLUSTER_NAME - Gardener cluster name
+# - CLUSTER_KUBECONFIG - Gardener cluster kubeconfig path
+
+set -eo pipefail
+
+if [ $# -lt 1 ]; then
+    >&2 echo "Make target is required as parameter"
+    exit 1
+fi
+
+function check_required_vars() {
+  local requiredVarMissing=false
+  for var in "$@"; do
+    if [ -z "${!var}" ]; then
+      >&2 echo "Environment variable ${var} is required but not set"
+      requiredVarMissing=true
+    fi
+  done
+  if [ "${requiredVarMissing}" = true ] ; then
+    exit 2
+  fi
+}
+
+requiredVars=(
+    IMG
+    CLUSTER_NAME
+    CLUSTER_KUBECONFIG
+)
+
+check_required_vars "${requiredVars[@]}"
+
+echo "Executing tests in cluster ${CLUSTER_NAME}, kubeconfig ${CLUSTER_KUBECONFIG}"
+export KUBECONFIG="${CLUSTER_KUBECONFIG}"
+
+export CLUSTER_DOMAIN=$(kubectl get configmap -n kube-system shoot-info -o jsonpath="{.data.domain}")
+echo "Cluster domain: ${CLUSTER_DOMAIN}"
+
+export GARDENER_PROVIDER=$(kubectl get configmap -n kube-system shoot-info -o jsonpath="{.data.provider}")
+echo "Gardener provider: ${GARDENER_PROVIDER}"
+
+export TEST_DOMAIN="${CLUSTER_DOMAIN}"
+
+# Add pwd to path to be able to use binaries downloaded in scripts
+export PATH="${PATH}:${PWD}"
+
+echo "Deploying Istio module, image: ${IMG}"
+make deploy
+
+cd tests/performance || exit
+echo "Executing tests..."
+echo "Executing make target test-performance-web"
+make test-performance-web
+echo "Tests finished"
+
+

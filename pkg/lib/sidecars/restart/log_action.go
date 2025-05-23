2ccae9cb40712bcc673bb30d5d0cadfa3b6f24d5
Bartosz Chwila <103247439+barchw@users.noreply.github.com>
2025-05-23 11:38:19 +0200

    fix: Warning should not happen when ReplicaSet is not ready (#1447)
    * fix: Warning should not happen when ReplicaSet is not ready
    
    * Make channel unbuffered

---
+++ pkg/lib/sidecars/restart/log_action.go
@@ -0,0 +1,18 @@
+package restart
+
+import (
+	"context"
+
+	"github.com/go-logr/logr"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+type logAction struct {
+	message string
+}
+
+func (r logAction) run(_ context.Context, _ client.Client, object actionObject, l *logr.Logger) ([]RestartWarning, error) {
+	l.Info(r.message, "kind", object.Kind, "name", object.Name, "namespace", object.Namespace)
+	return []RestartWarning{}, nil
+}

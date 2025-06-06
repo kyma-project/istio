38d4c9deabc6e49e04e868b8c40860293b3a7397
Luk, Marek <marek.luk@sap.com>
2025-06-06 11:18:49 +0200

    Fixed lint issue

--- internal/restarter/predicates/image_resources.go
+++ internal/restarter/predicates/image_resources.go
@@ -87,7 +87,8 @@ func hasCustomImageAnnotation(pod v1.Pod) bool {
 }
 
 func hasSidecarContainerWithWithDifferentImage(pod v1.Pod, expectedImage SidecarImage) bool {
-	c := append(pod.Spec.Containers, pod.Spec.InitContainers...)
+	c := pod.Spec.Containers
+	c = append(c, pod.Spec.InitContainers...)
 	for _, container := range c {
 		if isContainerIstioSidecar(container) && !expectedImage.matchesImageIn(container) {
 			return true

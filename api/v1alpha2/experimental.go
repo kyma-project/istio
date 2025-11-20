4a538c81fbcd22b6a9c0998eaefd6c2da823aa19
Marek Luk <40407813+mluk-sap@users.noreply.github.com>
2025-11-20 10:46:16 +0100

    Experimental support for IPv6 (#1700)
    Experimental support for IPv6

--- api/v1alpha2/experimental.go
+++ api/v1alpha2/experimental.go
@@ -2,6 +2,10 @@ package v1alpha2
 
 type Experimental struct {
 	PilotFeatures `json:"pilot"`
+
+	// Enables the Dual Stack support
+	// +kubebuilder:validation:Optional
+	EnableDualStack *bool `json:"enableDualStack,omitempty"`
 }
 
 type PilotFeatures struct {

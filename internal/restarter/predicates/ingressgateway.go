8373bca80abca70eca15c4767b2a1b36f49e26b9
Robert Kubica <168091212+kubicar@users.noreply.github.com>
2025-12-02 12:52:29 +0100

    added restarter

--- internal/restarter/predicates/ingressgateway.go
+++ internal/restarter/predicates/ingressgateway.go
@@ -21,10 +21,31 @@ func (i RestartPredicate) NewIngressGatewayEvaluator(_ context.Context) (Ingress
 		return nil, err
 	}
 
-	return NumTrustedProxiesRestartEvaluator{
-		NewNumTrustedProxies: i.istioCR.Spec.Config.NumTrustedProxies,
-		OldNumTrustedProxies: lastAppliedConfig.Config.NumTrustedProxies,
+	return CompositeIngressGatewayRestartEvaluator{
+		Evaluators: []IngressGatewayRestartEvaluator{
+			NumTrustedProxiesRestartEvaluator{
+				NewNumTrustedProxies: i.istioCR.Spec.Config.NumTrustedProxies,
+				OldNumTrustedProxies: lastAppliedConfig.Config.NumTrustedProxies,
+			},
+			TrustDomainsRestartEvaluator{
+				NewTrustedDomains: i.istioCR.Spec.Config.TrustDomain,
+				OldTrustedDomains: lastAppliedConfig.Config.TrustDomain,
+			},
+		},
 	}, nil
+}
+
+type CompositeIngressGatewayRestartEvaluator struct {
+	Evaluators []IngressGatewayRestartEvaluator
+}
+
+func (c CompositeIngressGatewayRestartEvaluator) RequiresIngressGatewayRestart() bool {
+	for _, evaluator := range c.Evaluators {
+		if evaluator.RequiresIngressGatewayRestart() {
+			return true
+		}
+	}
+	return false
 }
 
 type NumTrustedProxiesRestartEvaluator struct {
@@ -41,5 +62,22 @@ func (i NumTrustedProxiesRestartEvaluator) RequiresIngressGatewayRestart() bool 
 		return true
 	}
 
+	return false
+}
+
+type TrustDomainsRestartEvaluator struct {
+	NewTrustedDomains *string
+	OldTrustedDomains *string
+}
+
+func (i TrustDomainsRestartEvaluator) RequiresIngressGatewayRestart() bool {
+	isNewNotNil := i.NewTrustedDomains != nil
+	isOldNotNil := i.OldTrustedDomains != nil
+	if isNewNotNil && isOldNotNil && *i.NewTrustedDomains != *i.OldTrustedDomains {
+		return true
+	}
+	if isNewNotNil != isOldNotNil {
+		return true
+	}
 	return false
 }

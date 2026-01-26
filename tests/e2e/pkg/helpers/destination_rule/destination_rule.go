f1db0a30463672c4b7e32cd577e29c8ab36b8c86
Marek Kolodziejczak <marek.kolodziejczak@sap.com>
2026-01-26 21:43:37 +0100

    destination rule in helper and httpbin asserter for has proxy or no

---
+++ tests/e2e/pkg/helpers/destination_rule/destination_rule.go
@@ -0,0 +1,61 @@
+package destination_rule
+
+import (
+	"testing"
+
+	apinetworkingv1 "istio.io/api/networking/v1"
+	networkingv1 "istio.io/client-go/pkg/apis/networking/v1"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/kyma-project/istio/operator/tests/e2e/pkg/helpers/client"
+	"github.com/kyma-project/istio/operator/tests/e2e/pkg/setup"
+)
+
+// CreateDestinationRule creates a destination rule and registers cleanup
+func CreateDestinationRule(t *testing.T, name, namespace, host string) (*networkingv1.DestinationRule, error) {
+	t.Helper()
+	t.Logf("creating destination rule %s/%s", namespace, name)
+
+	r, err := client.ResourcesClient(t)
+	if err != nil {
+		t.Logf("Failed to get resources client: %v", err)
+		return nil, err
+	}
+
+	destinationRule := &networkingv1.DestinationRule{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "networking.istio.io/v1",
+			Kind:       "DestinationRule",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Spec: apinetworkingv1.DestinationRule{
+			Host: host,
+		},
+	}
+
+	err = r.Create(t.Context(), destinationRule)
+	if err != nil {
+		if !k8serrors.IsAlreadyExists(err) {
+			t.Logf("Failed to create destination rule: %v", err)
+			return nil, err
+		}
+		t.Logf("destination rule %s/%s already exists", namespace, name)
+	} else {
+		t.Logf("destination rule %s/%s created", namespace, name)
+	}
+
+	setup.DeclareCleanup(t, func() {
+		err := r.Delete(setup.GetCleanupContext(), destinationRule)
+		if err != nil && !k8serrors.IsNotFound(err) {
+			t.Logf("Failed to delete destination rule: %v", err)
+		} else {
+			t.Logf("destination rule %s/%s deleted", namespace, name)
+		}
+	})
+
+	return destinationRule, nil
+}
--- tests/e2e/tests/installation/uninstallation_test.go
+++ tests/e2e/tests/installation/uninstallation_test.go
@@ -6,12 +6,8 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/require"
-	apinetworkingv1 "istio.io/api/networking/v1"
-	networkingv1 "istio.io/client-go/pkg/apis/networking/v1"
 	v1 "k8s.io/api/core/v1"
-	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"sigs.k8s.io/e2e-framework/klient/k8s/resources"
 	"sigs.k8s.io/e2e-framework/klient/wait"
 	"sigs.k8s.io/e2e-framework/klient/wait/conditions"
 
@@ -19,13 +15,14 @@ import (
 
 	"github.com/kyma-project/istio/operator/api/v1alpha2"
 	"github.com/kyma-project/istio/operator/internal/reconciliations/istio"
+	httpbinassert "github.com/kyma-project/istio/operator/tests/e2e/pkg/asserts/httpbin"
 	istioassert "github.com/kyma-project/istio/operator/tests/e2e/pkg/asserts/istio"
 	"github.com/kyma-project/istio/operator/tests/e2e/pkg/helpers/client"
 	"github.com/kyma-project/istio/operator/tests/e2e/pkg/helpers/crds"
+	"github.com/kyma-project/istio/operator/tests/e2e/pkg/helpers/destination_rule"
 	"github.com/kyma-project/istio/operator/tests/e2e/pkg/helpers/httpbin"
 	modulehelpers "github.com/kyma-project/istio/operator/tests/e2e/pkg/helpers/modules"
 	"github.com/kyma-project/istio/operator/tests/e2e/pkg/helpers/namespace"
-	"github.com/kyma-project/istio/operator/tests/e2e/pkg/setup"
 )
 
 func TestUninstall(t *testing.T) {
@@ -48,17 +45,8 @@ func TestUninstall(t *testing.T) {
 		_, err = httpbin.NewBuilder().WithName("httpbin-regular-sidecar").WithNamespace("default").WithRegularSidecar().DeployWithCleanup(t)
 		require.NoError(t, err)
 
-		httpbinPodList := &v1.PodList{}
-		err = c.List(t.Context(), httpbinPodList, resources.WithLabelSelector("app=httpbin"))
-		require.NoError(t, err)
-		require.NotEmpty(t, httpbinPodList.Items, "httpbin pod not found")
-		require.True(t, hasIstioProxy(httpbinPodList.Items[0]), "httpbin pod should have istio-proxy")
-
-		httpbinRegularPodList := &v1.PodList{}
-		err = c.List(t.Context(), httpbinRegularPodList, resources.WithLabelSelector("app=httpbin-regular-sidecar"))
-		require.NoError(t, err)
-		require.NotEmpty(t, httpbinRegularPodList.Items, "httpbin-regular-sidecar pod not found")
-		require.True(t, hasIstioProxy(httpbinRegularPodList.Items[0]), "httpbin-regular-sidecar pod should have istio-proxy")
+		httpbinassert.AssertIstioProxyPresent(t, c, "app=httpbin")
+		httpbinassert.AssertIstioProxyPresent(t, c, "app=httpbin-regular-sidecar")
 
 		istioNs := &v1.Namespace{}
 		err = c.Get(t.Context(), "istio-system", "", istioNs)
@@ -80,31 +68,8 @@ func TestUninstall(t *testing.T) {
 		}), wait.WithTimeout(2*time.Minute))
 		require.NoError(t, err)
 
-		err = wait.For(func(ctx context.Context) (bool, error) {
-			httpbinPodList := &v1.PodList{}
-			err := c.List(ctx, httpbinPodList, resources.WithLabelSelector("app=httpbin"))
-			if err != nil {
-				return false, err
-			}
-			if len(httpbinPodList.Items) == 0 {
-				return false, nil
-			}
-			return !hasIstioProxy(httpbinPodList.Items[0]), nil
-		}, wait.WithTimeout(2*time.Minute), wait.WithContext(t.Context()))
-		require.NoError(t, err)
-
-		err = wait.For(func(ctx context.Context) (bool, error) {
-			httpbinRegularPodList := &v1.PodList{}
-			err := c.List(ctx, httpbinRegularPodList, resources.WithLabelSelector("app=httpbin-regular-sidecar"))
-			if err != nil {
-				return false, err
-			}
-			if len(httpbinRegularPodList.Items) == 0 {
-				return false, nil
-			}
-			return !hasIstioProxy(httpbinRegularPodList.Items[0]), nil
-		}, wait.WithTimeout(2*time.Minute), wait.WithContext(t.Context()))
-		require.NoError(t, err)
+		httpbinassert.AssertIstioProxyAbsent(t, c, "app=httpbin")
+		httpbinassert.AssertIstioProxyAbsent(t, c, "app=httpbin-regular-sidecar")
 	})
 
 	t.Run("Uninstallation respects the Istio resources created by the user", func(t *testing.T) {
@@ -117,28 +82,9 @@ func TestUninstall(t *testing.T) {
 		istioCR, err := modulehelpers.NewIstioCRBuilder().ApplyAndCleanup(t)
 		require.NoError(t, err)
 
-		destinationRule := &networkingv1.DestinationRule{
-			TypeMeta: metav1.TypeMeta{
-				APIVersion: "networking.istio.io/v1",
-				Kind:       "DestinationRule",
-			},
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "customer-destination-rule",
-				Namespace: "default",
-			},
-			Spec: apinetworkingv1.DestinationRule{
-				Host: "testing-svc.default.svc.cluster.local",
-			},
-		}
-		err = c.Create(t.Context(), destinationRule)
+		destinationRule, err := destination_rule.CreateDestinationRule(t, "customer-destination-rule", "default", "testing-svc.default.svc.cluster.local")
 		require.NoError(t, err)
 
-		setup.DeclareCleanup(t, func() {
-			err := c.Delete(setup.GetCleanupContext(), destinationRule)
-			if err != nil && !k8serrors.IsNotFound(err) {
-				t.Logf("Failed to delete destination rule: %v", err)
-			}
-		})
 
 		err = c.Delete(t.Context(), istioCR)
 		require.NoError(t, err)
@@ -229,11 +175,3 @@ func TestUninstall(t *testing.T) {
 
 }
 
-func hasIstioProxy(pod v1.Pod) bool {
-	for _, container := range append(pod.Spec.Containers, pod.Spec.InitContainers...) {
-		if container.Name == "istio-proxy" {
-			return true
-		}
-	}
-	return false
-}

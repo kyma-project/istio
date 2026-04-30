91880013064b141c2c807b03857ddea3f2d88038
Bartosz Chwila <103247439+barchw@users.noreply.github.com>
2026-04-30 10:43:23 +0200

    Add conditional support for VPAs (#2020)
    * Add conditional support for VPAs (#2017)
    
    * Add conditional support for VPAs
    
    * Add Relase notes
    
    * Apply suggestions from code review
    
    Co-authored-by: Natalia Sitko <80401180+nataliasitko@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Natalia Sitko <80401180+nataliasitko@users.noreply.github.com>
    
    * Update RN
    
    ---------
    
    Co-authored-by: Natalia Sitko <80401180+nataliasitko@users.noreply.github.com>

--- api/v1alpha2/compatibility_mode.go
+++ api/v1alpha2/compatibility_mode.go
@@ -7,7 +7,7 @@ import (
 
 //nolint:gochecknoglobals // vairables are used to set compatibility mode for IstioOperator TODO: refactor to constants
 var (
-	PilotCompatibilityEnvVars = map[string]string{}
+	PilotCompatibilityEnvVars  = map[string]string{}
 	ProxyMetaDataCompatibility = map[string]string{}
 )
 

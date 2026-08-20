23ee469fade1e2753235e6e871a97664704fdd4b
Marek Kolodziejczak <marek.kolodziejczak@sap.com>
2026-08-20 11:42:26 +0200

    Review suggestions

--- api/v1alpha2/istio_structs.go
+++ api/v1alpha2/istio_structs.go
@@ -49,7 +49,7 @@ type Config struct {
 
 	// Configures which Istio proxy stats are emitted by matching stat names against inclusion regular expressions.
 	// Stats whose names do not match any of the configured inclusion patterns are not emitted by the proxy.
-	// For more information, see https://istio.io/latest/docs/ops/configuration/telemetry/envoy-stats/
+	// For more information, see [Envoy Statistics](https://istio.io/latest/docs/ops/configuration/telemetry/envoy-stats/).
 	// +kubebuilder:validation:Optional
 	ProxyStatsMatcher *ProxyStatsMatcher `json:"proxyStatsMatcher,omitempty"`
 }

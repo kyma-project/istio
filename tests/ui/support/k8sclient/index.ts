3205318c3902d10f1f5854d7ccbd1c073a191168
Kamil Kasperski <kamil.kasperski@sap.com>
2026-09-10 12:59:02 +0200

    chore: ui: remove UI tests (#2210)

--- tests/ui/support/k8sclient/index.ts
+++
@@ -1,7 +0,0 @@
-import "./authorizationPolicy"
-import "./namespace"
-import "./service"
-import "./pod";
-export {Commands as K8sClientCommands} from "./commands";
-export {KubernetesConfig, getK8sCurrentContext} from "./kubeconfig";
-

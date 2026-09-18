11f19dd1d38312cf1e16d1de668bd976c741ecbb
I531931 <kamil.kasperski@sap.com>
2026-09-18 15:13:12 +0200

    test: plug new rules to old restarter

--- pkg/lib/sidecars/pods/get_test.go
+++ pkg/lib/sidecars/pods/get_test.go
@@ -273,7 +273,7 @@ var _ = Describe("GetPodsToRestart", func() {
 				tt.predicates = append(tt.predicates, predicates.NewImageResourcesPredicate(expectedImage, helpers.DefaultSidecarResources))
 				podsLister := pods.NewPods(tt.c, &logger)
 				var collected v1.PodList
-				err := podsLister.GetPodsToRestart(ctx, tt.predicates, tt.limits, func(_ context.Context, page *v1.PodList) error {
+				err := podsLister.GetPodsToRestart(ctx, tt.predicates, nil, tt.limits, func(_ context.Context, page *v1.PodList) error {
 					collected.Items = append(collected.Items, page.Items...)
 					return nil
 				})
@@ -429,7 +429,7 @@ var _ = Describe("GetPodsToRestart", func() {
 				expectedImage := images.Image{Registry: "istio", Name: "proxyv2", Tag: "1.10.0"}
 				podsLister := pods.NewPods(tt.c, &logger)
 				var collected v1.PodList
-				err := podsLister.GetPodsToRestart(ctx, []predicates.SidecarProxyPredicate{predicates.NewImageResourcesPredicate(expectedImage, helpers.DefaultSidecarResources)}, pods.NewPodsRestartLimits(5), func(_ context.Context, page *v1.PodList) error {
+				err := podsLister.GetPodsToRestart(ctx, []predicates.SidecarProxyPredicate{predicates.NewImageResourcesPredicate(expectedImage, helpers.DefaultSidecarResources)}, nil, pods.NewPodsRestartLimits(5), func(_ context.Context, page *v1.PodList) error {
 					collected.Items = append(collected.Items, page.Items...)
 					return nil
 				})
